1bf6f1f221587447d114adf2e8fbbd886d8d1773
Lee Tze Cheun <leetzecheun@gmail.com>
2011-08-31 16:59:10 +0800

    - Replaced variable logo with a helper method. - Added tests to check that links go to the right pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
   
+  # Return the logo image.
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
+  
   # Return a title on a per-page basis.
   def title
     base_title = "Ruby on Rails Tutorial Sample App"    
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,4 +27,22 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Sign Up")
   end
   
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+  
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")  
+    
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign Up")
+  end
+  
 end
