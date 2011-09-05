cd7950ef135a048e4cdde645ba2fe1fc5e17910e
Lee Tze Cheun <leetzecheun@gmail.com>
2011-09-05 15:55:33 +0800

    Refactored integration specs to use integration_sign_in function instead.

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -56,10 +56,7 @@ describe "LayoutLinks" do
   describe "when signed in" do    
     before :each do
       @user = Factory(:user)
-      visit signin_path
-      fill_in :email, :with => @user.email
-      fill_in :password, :with => @user.password
-      click_button
+      integration_sign_in @user
     end
     
     it "should have a sign out link" do
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -38,21 +38,17 @@ describe "Users" do
   describe "sign in/out" do    
     describe "failure" do
       it "should not sign a user in" do
-        visit signin_path
-        fill_in :email, :with => ""
-        fill_in :password, :with => ""
-        click_button
+        user = Factory(:user)
+        user.email = ""
+        user.password = ""
+        integration_sign_in user        
         response.should have_selector("div.flash.error", :content => "Invalid")
       end
     end
     
     describe "success" do
       it "should sign a user in and out" do
-        user = Factory(:user)
-        visit signin_path
-        fill_in :email, :with => user.email
-        fill_in :password, :with => user.password
-        click_button
+        integration_sign_in Factory(:user)        
         controller.should be_signed_in
         click_link "Sign Out"
         controller.should_not be_signed_in
