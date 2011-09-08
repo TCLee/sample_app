9c242c91a16c2236a73ef57edc12e8c33a8d3482
Lee Tze Cheun <leetzecheun@gmail.com>
2011-09-08 15:13:16 +0800

    Added JavaScript code to count down from 140 characters.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+/* Count the number of characters in a micropost. */
+function countCharacters(textField, charCountLabel, maxLength) {
+  var currentLength = textField.value.length;
+  if (currentLength > maxLength) {
+    // Trim the text, if it exceeds max length.
+    textField.value = textField.value.substring(0, maxLength);
+  } else {
+    // Update characters remaining counter label.
+    charCountLabel.innerHTML = maxLength - currentLength;
+  }
+}
