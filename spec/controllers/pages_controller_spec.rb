2eb523b65fdb39c9caccd06948d4bf687e649e3c
Lee Tze Cheun <leetzecheun@gmail.com>
2011-09-08 15:59:11 +0800

    Added tests for the sidebar micropost counts.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
-  before(:each) do
+  before :each do
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
@@ -17,6 +17,25 @@ describe PagesController do
       get 'home'
       response.should have_selector("title", 
         :content => @base_title + " | Home")
+    end
+    
+    describe "sidebar micropost count" do
+      before :each do
+        @user = Factory(:user)
+        @micropost1 = Factory(:micropost, :user => @user)
+        test_sign_in(@user)
+      end
+      
+      it "should be singular" do
+        get 'home'
+        response.should have_selector("span.microposts", :content => "1 micropost")
+      end
+      
+      it "should be plural" do
+        micropost2 = Factory(:micropost, :user => @user)
+        get 'home'
+        response.should have_selector("span.microposts", :content => "2 microposts")
+      end
     end
   end
 
